ead19d898cc112111fda1129f1d8b15e23bd8b13
Lawrence137 <lawrencekimani18@gmail.com>
2022-11-23 15:26:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value){
+  if (value <= 400){
+    return 'This one is on me!'
+  }
+  else if (value > 2000 && value < 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (value > 2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
 
-function switchOnCharmFromTip(){
+let result
+ result = city == 'NYC' ? 'Ok, sounds good.' : 'No go.'
+ return result
+ }
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    case 'thanks for everything':
+      return 'Bye.';
+      break;
+      }
 }
